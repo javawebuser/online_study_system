cc074567f43c1038c4abba44b2bcb425eaabcfcc
chenlong <3215712411@qq.com>
2019-11-01 00:28:36 +0800

    index

--- src/main/java/com/cqgcxy/online_study_system/controller/userController.java
+++ src/main/java/com/cqgcxy/online_study_system/controller/userController.java
@@ -1,5 +1,6 @@
 package com.cqgcxy.online_study_system.controller;
 
+import com.cqgcxy.online_study_system.entity.ResultMsg;
 import com.cqgcxy.online_study_system.entity.User;
 import com.cqgcxy.online_study_system.service.userService;
 import org.apache.shiro.SecurityUtils;
@@ -12,8 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 public class userController {
@@ -26,8 +33,11 @@ public class userController {
         return "/login";
     }
 
+
     @RequestMapping("/user")
-    public String username(String username,String password,Model model){
+    public String username(@RequestParam("username") String username,
+                           @RequestParam("password") String password,
+                           Model model,HttpSession session){
         /**
          * 使用Shiro编写认证操作
          */
@@ -39,25 +49,26 @@ public class userController {
         //3.执行登录方法
         try {
             subject.login(token);
+            User user=(User)subject.getPrincipal();
+            session.setAttribute("loginUser",user);
             //登录成功
             return "redirect:/index";
         } catch (UnknownAccountException e) {
             //登录失败
-            model.addAttribute("msg","用户名不存在");
-            return "redirect:/login";
+            model.addAttribute("msg", "用户不存在");
+            return "login";
         }catch (IncorrectCredentialsException e) {
             //登录失败
-            model.addAttribute("msg","密码错误");
-            return "redirect:/login";
+            model.addAttribute("msg", "用户名密码错误");
+            return "login";
         }
 
     }
 
-
     @RequestMapping("/index")
-    public ModelAndView index(){
-        ModelAndView modelAndView = new ModelAndView("/index");
-        return modelAndView;
+    public ModelAndView index(HttpServletRequest request){
+        ModelAndView mv = new ModelAndView("/index");
+        return mv;
     }
 
     @RequestMapping("/add")
@@ -87,5 +98,21 @@ public class userController {
     @RequestMapping("/authorizedUrl")
     public String authorizedUrl(){
         return "/authorizedUrl";
+    }
+
+    @RequestMapping("/welcome")
+    public  String welcome() {
+        return "/welcome";
+    }
+
+    @ResponseBody
+    @RequestMapping("/exit_submit")
+    public ResultMsg exit_submit(HttpServletRequest request,String name) {
+        if(name.equals("11")) {
+            HttpSession session=request.getSession();
+            session.invalidate();
+            return new ResultMsg(1, "退出!");
+        }
+        return new ResultMsg(0, "退出失败!");
     }
 }
